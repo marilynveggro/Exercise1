e852ddb86e86fec0f3d1ee55475f27f04dff86ba
Marilyn <mveggro@gmail.com>
2022-12-09 13:07:46 +1300

    basic codes

---
+++ Exercise1.py
@@ -0,0 +1,46 @@
+print("Hello World")
+age = 20
+print(age)
+price = 19.95
+print(price)
+first_name = 'marilyn'
+print(first_name)
+is_online = False
+name = input('What is your Name? ')
+print('Hello ' + name)
+### sum
+first = float(input('First Number: '))
+second = float(input('second Number: '))
+sum = first + second
+print('sum:'+str(sum))
+
+
+
+
+lista = [1, 2, 8, 12, 15,17]
+contiguos = []
+
+#iteramos en el rango del tamaño de la lista -1
+for element in range(len(lista)-1):
+    #un numero es contiguo con otro si sumado o restado 1 son iguales
+    if lista[element] == lista[element+1]-1:
+        contiguos.append((lista[element],lista[element+1]))
+print(contiguos)
+
+
+
+
+#Generates a new  user password
+import random
+
+chars = "abcdefghijklmnopqrstuvwxzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+newPassword = []
+for x in range(8):
+    newPassword.append(random.choice(chars))
+    finalPassword = ''.join(map(str, newPassword))
+print("\nThis is your new password: ",finalPassword)
+new_password = input("Please enter your New Password: ")
+
+problem = input("\nEnter description of problem: ")
+
+############################
